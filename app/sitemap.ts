43c300372e3bd53f46ce99ae92d1cbc201314557
Sebastian Linconao <sebaxlinconao@gmail.com>
2026-07-28 16:22:33 -0400

    Actualizo URL del sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://altos-de-botrolhue.vercel.app",
+      url: "https://altos-de-botrolhue.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
