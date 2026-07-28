c606a636c4c2ee8946db46d375ab246c04147eec
Sebastian Linconao <sebaxlinconao@gmail.com>
2026-07-27 22:44:54 -0400

    Agrego sitemap para Google

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://altos-de-botrolhue.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+}
